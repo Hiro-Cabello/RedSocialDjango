73882d10a5b9f91deecd424162049791671d9aa5
Hiro-Cabello <ronaljairocabellocordova@gmail.com>
2021-11-24 17:24:21 -0500

    deploy-3

--- blog/settings.py
+++ blog/settings.py
@@ -188,8 +188,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+
+
+
+#STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILE_DIRS = [
+      "static/images", 
+      "static/css", 
+      "staticfiles",]
+
+
+
 
 AUTH_USER_MODEL = 'users.User'
 
